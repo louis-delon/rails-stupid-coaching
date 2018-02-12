9e6928a841ad14020f497870e98353b0fcd32210
louis delon <louleman34@gmail.com>
2018-02-12 15:30:17 +0100

    exo1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,25 @@
 class QuestionsController < ApplicationController
-  def ask
+
+  # fournit un formulaire html qui pertmet de poser sa question.
+  # def ask
+  # end
+
+  # reçoit une question, l'affiche avec la réponse adaptée.
+  def answer
+    @question = params[:question]
+    generate_answer
   end
 
-  def answer
+  private
+
+  def generate_answer
+    # @answer = nil
+    if @question == "I am going to work"
+      @answer = "great"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
